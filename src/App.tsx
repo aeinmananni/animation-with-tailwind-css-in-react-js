d332a69b2848c78f524bba3a02d86175207b46b8
aein <mananniaein1234@gmail.com>
2024-10-10 13:53:58 +0330

    update new Hover Button Animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 const App = () => {
   return (
-    <div className="floatingPieceBefore floatingPieceAfter border p-5 rounded-lg border-gray-500 overflow-hidden hover:text-white duration-300 relative">
+    // <div className="floatingPieceBefore floatingPieceAfter border p-5 rounded-lg border-gray-500 overflow-hidden hover:text-white duration-300 relative">
+    //   <span>hover me</span>
+    // </div>
+    // <div className="boldButtonWithHover border p-5 rounded-lg border-gray-500 overflow-hidden  hover:text-orange-500 hover:font-bold hover:-tracking-tighter  duration-500 relative">
+    //   <span>hover me</span>
+    // </div>
+    <div className="addBforeElementText overflow-hidden border p-5 rounded-lg border-gray-500   duration-500 relative ">
       <span>hover me</span>
     </div>
   );
