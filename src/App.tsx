01846c6b2cace2c537f82299893637b9651ee4ec
aein <mananniaein1234@gmail.com>
2024-10-12 10:10:35 +0330

    new update Button Effect

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ const App = () => {
     // <div className="boldButtonWithHover border p-5 rounded-lg border-gray-500 overflow-hidden  hover:text-orange-500 hover:font-bold hover:-tracking-tighter  duration-500 relative">
     //   <span>hover me</span>
     // </div>
-    <div className="addBforeElementText overflow-hidden border p-5 rounded-lg border-gray-500   duration-500 relative ">
+    // <div className="addBforeElementText overflow-hidden border p-5 rounded-lg border-gray-500   duration-500 relative ">
+    //   <span>hover me</span>
+    // </div>
+    <div className="hover-offset-button overflow-hidden border p-5 rounded-lg border-gray-500  hover:bg-red-500  duration-300 relative ">
       <span>hover me</span>
     </div>
   );
