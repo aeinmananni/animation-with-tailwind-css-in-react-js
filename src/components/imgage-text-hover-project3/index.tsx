469b4fcceb7e5394ded381c63bd35367aebff5b9
aein <mananniaein1234@gmail.com>
2024-11-10 08:25:04 +0330

    animation new Project

--- src/components/imgage-text-hover-project3/index.tsx
+++ src/components/imgage-text-hover-project3/index.tsx
@@ -28,6 +28,18 @@ const ImageTextHoverProject3 = () => {
           John-Wick4_Movies
         </span>
       </div>
+      <div
+        className={`${styles.ImageHoverd3} w-80 h-80 duration-500 border overflow-hidden rounded-lg relative group flex justify-center `}
+      >
+        <img
+          src={images2}
+          alt=""
+          className="h-full object-cover rounded-lg group-hover:translate-x-[200%] translate-x-0 duration-500 group-hover:scale-110 absolute"
+        />
+        <span className="absolute z-30 w-full text-white left-[13%] font-bold text-2xl top-12 opacity-0 group-hover:opacity-100  duration-300 delay-100">
+          John-Wick4_Movies
+        </span>
+      </div>
     </div>
   );
 };
