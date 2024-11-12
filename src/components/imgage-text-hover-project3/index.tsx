570c6d94709e570c6c60b2769abf5a3c837c3fac
aein <mananniaein1234@gmail.com>
2024-11-12 10:18:37 +0330

    Add New Animation In animation project

--- src/components/imgage-text-hover-project3/index.tsx
+++ src/components/imgage-text-hover-project3/index.tsx
@@ -29,14 +29,26 @@ const ImageTextHoverProject3 = () => {
         </span>
       </div>
       <div
-        className={`${styles.ImageHoverd3} w-80 h-80 duration-500 border overflow-hidden rounded-lg relative group flex justify-center `}
+        className={`${styles.ImageHoverd3} bg-sky-900 w-80 h-80 duration-500 border overflow-hidden rounded-lg relative group flex justify-center `}
       >
         <img
           src={images2}
           alt=""
-          className="h-full object-cover rounded-lg group-hover:translate-x-[200%] translate-x-0 duration-500 group-hover:scale-110 absolute"
+          className="h-full object-cover rounded-lg group-hover:translate-x-[200%] translate-x-0 duration-500  absolute"
         />
         <span className="absolute z-30 w-full text-white left-[13%] font-bold text-2xl top-12 opacity-0 group-hover:opacity-100  duration-300 delay-100">
+          John-Wick4_Movies
+        </span>
+      </div>
+      <div
+        className={`${styles.ImageHoverd4} bg-sky-900 w-80 h-80 duration-500 border overflow-hidden rounded-lg relative group flex justify-center `}
+      >
+        <img
+          src={images2}
+          alt=""
+          className="h-full object-cover rounded-lg group-hover:-translate-y-20 translate-y-0 duration-500  absolute"
+        />
+        <span className="absolute z-30 w-full text-white left-[13%] font-bold text-2xl -bottom-10 group-hover:bottom-5  group-hover:opacity-100  duration-300 delay-100">
           John-Wick4_Movies
         </span>
       </div>
